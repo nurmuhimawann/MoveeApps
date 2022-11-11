96737e253f4ba8fea9a68e3d4aabccd622eed230
Nur Muhammad Himawan <muhammad.himawan73@gmail.com>
2022-11-12 04:01:45 +0700

    minifying code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# MoveeApps
+# MoveeApps - MLFE: Belajar Fundamental Front-End Web Development
+
+Proyek submission untuk kelas **Belajar Fundamental Front-End Web Development**.
+
+<div align="center">
+<figure>
+    <img src ="https://github.com/nurmuhimawann/MoveeApps/blob/main/src/assets/img/moveee.png?raw=true" alt="moveeapps">
+    <figcaption align="center"><b>Display Front-End</b></figcaption>
+</figure>
+</div>
+<br>
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,16 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require("terser-webpack-plugin")
 
 module.exports = merge(common, {
     mode: 'production',
+    plugins: [
+        new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
+        new CleanWebpackPlugin(),
+    ],
     module: {
         rules: [
             {
@@ -12,11 +20,24 @@ module.exports = merge(common, {
                     {
                         loader: 'babel-loader',
                         options: {
-                            presets: ['@babel/preset-env']
-                        }
-                    }
-                ]
-            }
-        ]
-    }
-})
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new CssMinimizerPlugin(),
+            (compiler) => {
+                new TerserPlugin({
+                    terserOptions: {
+                        compress: {},
+                    },
+                }).apply(compiler);
+            },
+        ],
+    },
+});
